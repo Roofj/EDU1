11ac992b4bc062ae2aece16928c6d7dd795f327b
Nikita <nikita.suvorov.1998@gmail.com>
2020-11-04 22:15:10 +0400

    add methods

--- src/Kotik.java
+++ src/Kotik.java
@@ -1,4 +1,40 @@
 public class Kotik
 {
-
+    int hungry = 5;
+    int AP = 4;
+    String name = new String ("Kotik");
+    public void eat ()
+    {
+        this.hungry=+3;
+        this.AP=-1;
+    }
+    public void play()
+    {
+        this.hungry=-2;
+        this.AP=-1;
+    }
+    public void sleep()
+    {
+        this.hungry=-1;
+        this.AP=+5;
+    }
+    public void walk()
+    {
+        this.hungry=-2;
+        this.AP=-1;
+    }
+    public void chaseMouse()
+    {
+        this.hungry=-3;
+        this.AP=-1;
+    }
+    public void dead()
+    {
+        System.out.println(this.name + " is dead");
+    }
+    public void relax()
+    {
+        this.hungry=-1;
+        this.AP=+1;
+    }
 }
